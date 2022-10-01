c8cf4b0992d82c05cf6eb149452f4d58a8abd883
Maurice Heinze <mheinze@2-I-5.42heilbronn.de>
2022-10-01 19:29:10 +0200

    line is split up by '|' now

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 // order string by types
 
 // reduces strings by reducing all delimiting chars in a row to one
-char *ft_strred(char *cmd_line)
+char	*ft_strred(char *cmd_line)
 {
 	int		i;
 	int		k;
@@ -44,13 +44,73 @@ char *ft_strred(char *cmd_line)
 				reduced_str[k++] = cmd_line[i];
 				if (cmd_line[i + 1] <= 32 && (i < end) && reduce)
 					reduced_str[k++] = cmd_line[i + 1];
-
 			}
+			reduced_str[k] = '\0';
 		}
 		i++;
 	}
 	return (reduced_str);
 }
+
+int		get_no_cmd_lines(char *input_str)
+{
+	int		i;
+	int		no_cmd_lines;
+
+	i = 0;
+	no_cmd_lines = 1;
+	while (input_str[i] != '\0')
+	{
+		if (input_str[i] == '|')
+			no_cmd_lines++;
+		i++;
+	}
+	return (no_cmd_lines);
+}
+
+char	**split_cmd_lines(char *input_str)
+{
+	int		i;
+	int		no_cmd_lines;
+	char	**cmd_lines;
+
+	i = 0;
+	cmd_lines = ft_split(input_str, '|');
+	while (cmd_lines[i] != NULL)
+	{
+		cmd_lines[i] = ft_strred(cmd_lines[i]);
+		i++;
+	}
+	return (cmd_lines);
+}
+
+// // creates tokens out of reduced cmd_line
+// char	**tokenizer(char *reduced_cmd_line)
+// {
+// 	int		cmd_i;
+// 	int		token_i;
+// 	int		tokens_k;
+// 	char	*token;
+// 	char	**tokens;
+
+// 	cmd_i = 0;
+// 	token_i = 0;
+// 	tokens_k = 0;
+// 	token = malloc(sizeof(token) * 10000);
+// 	tokens = malloc(sizeof(token) * 10000);
+
+// 	while (reduced_cmd_line[cmd_i] != '\0')
+// 	{
+// 		while (is_delimiter(reduced_cmd_line[cmd_i]) == 0)
+// 		{
+// 			tokens[tokens_k][token_i] = reduced_cmd_line[cmd_i];
+// 			token_i++;
+// 			cmd_i++;
+// 		}
+// 		tokens[tokens_k][token_i] = '\0';
+// 		tokens_k++;
+// 	}
+// }
 
 int	main(int argc, char *argv[])
 {
@@ -58,8 +118,21 @@ int	main(int argc, char *argv[])
 
 	// printf("%s\n", cmd_line);
 	// printf("%d\n", is_separator('|'));
-	printf("%s", ft_strred("  	Hallo   'meine   Welt'  test   "));
-	printf("abc");
+	// printf("%s", ft_strred("'  	Hallo   meine   Welt'  test   "));
+
+	char	**tab;
+	int		i;
+
+	i = 0;
+	tab = split_cmd_lines("echo Hallo Welt | touch file |");
+
+	if (!tab[0])
+		ft_putendl_fd("ok\n", 1);
+	while (tab[i] != NULL)
+	{
+		ft_putendl_fd(tab[i], 1);
+		i++;
+	}
 	// found_cmd(cmd_line);
 	return (0);
 }
