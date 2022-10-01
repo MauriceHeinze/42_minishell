51a0f1ab7d41fe2c5190425952801695e7717cb7
Maurice Heinze <mheinze@2-I-5.42heilbronn.de>
2022-10-01 17:18:29 +0200

    strred skips all spaces (chars lower than 32)

--- src/main.c
+++ src/main.c
@@ -12,42 +12,44 @@ char *ft_strred(char *cmd_line)
 	int		i;
 	int		k;
 	int		end;
-	char	*tmp;
+	char	*reduced_str;
 	int		reduce;
 
 	i = 0;
 	k = 0;
 	reduce = 1;
-	tmp = malloc(sizeof(char) * ft_strlen(cmd_line));
-	if (!tmp)
+	reduced_str = malloc(sizeof(char) * ft_strlen(cmd_line));
+	if (!reduced_str)
 		return (NULL);
 	while (cmd_line[i++] != '\0')
 	end = i - 1;
-	i = -1;
-	while (cmd_line[end] == ' ')
+	i = 0;
+	while (cmd_line[end] <= 32)
 		end--;
-	while (cmd_line[i++] == ' ');
+	while (cmd_line[i] <= 32)
+		i++;
 	while (cmd_line[i] != '\0')
 	{
+		// printf("Index	%d|	Char:	%c|	Reduce: %d\n", i, cmd_line[i], reduce);
 		if (cmd_line[i] == '\'' && reduce == 1)
 			reduce = 0;
 		else if (cmd_line[i] == '\'' && reduce == 0)
 			reduce = 1;
 		if (reduce == 0)
-			tmp[k++] = cmd_line[i];
+			reduced_str[k++] = cmd_line[i];
 		else if (reduce == 1)
 		{
-			if (cmd_line[i] != ' ')
+			if (cmd_line[i] > 32)
 			{
-				tmp[k++] = cmd_line[i];
-				if (cmd_line[i + 1] == ' ' && (i < end) && reduce)
-					tmp[k++] = cmd_line[i + 1];
+				reduced_str[k++] = cmd_line[i];
+				if (cmd_line[i + 1] <= 32 && (i < end) && reduce)
+					reduced_str[k++] = cmd_line[i + 1];
 
 			}
 		}
 		i++;
 	}
-	return (tmp);
+	return (reduced_str);
 }
 
 int	main(int argc, char *argv[])
@@ -56,7 +58,7 @@ int	main(int argc, char *argv[])
 
 	// printf("%s\n", cmd_line);
 	// printf("%d\n", is_separator('|'));
-	printf("%s", ft_strred("'    Hallo   meine   Welt  test   '"));
+	printf("%s", ft_strred("  	Hallo   'meine   Welt'  test   "));
 	printf("abc");
 	// found_cmd(cmd_line);
 	return (0);
