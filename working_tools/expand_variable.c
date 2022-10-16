6eb4acc0732700a51a20676de425d3e7d69f6c45
Maurice Heinze <mheinze@2-I-5.42heilbronn.de>
2022-10-16 13:15:26 +0200

    added support for tilde

--- working_tools/expand_variable.c
+++ working_tools/expand_variable.c
@@ -7,15 +7,23 @@ static int	is_uppercase(char c)
 	return (0);
 }
 
+static char	*expand_variable(char *input_str, int start, int i)
+{
+	char	*var_name;
+
+	var_name = ft_substr(input_str, start, i - start);
+	input_str = str_remove_word(input_str, start, i - start + 1);
+	input_str = str_add(input_str, getenv(var_name), start);
+	free(var_name);
+	return (input_str);
+}
+
 char	*expand_variables(char *input_str)
 {
 	int		i;
-	int		var_len;
 	int		start;
-	char	*var_name;
 
 	i = 0;
-	var_len = 0;
 	// get number of chars of variables
 	while (input_str[i] != '\0')
 	{
@@ -25,10 +33,16 @@ char	*expand_variables(char *input_str)
 			start = i;
 			while (input_str[i] != '\0' && input_str[i] != '/' && (ft_isalpha(input_str[i]) || ft_isalnum(input_str[i]) || input_str[i] == '_'))
 				i++;
-			var_name = ft_substr(input_str, start, i - start);
+			input_str = expand_variable(input_str, start, i);
+			i = 0;
+			start = 0;
+		}
+		else if (input_str[i] == '~' && (input_str[i] != '\0' || ft_strchr("/ 	", input_str[i])))
+		{
+			i++;
+			start = i;
 			input_str = str_remove_word(input_str, start, i - start + 1);
-			input_str = str_add(input_str, getenv(var_name), start);
-			free(var_name);
+			input_str = str_add(input_str, getenv("HOME"), start);
 			i = 0;
 			start = 0;
 		}
