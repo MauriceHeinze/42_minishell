4aa2b63f3176903a01314242cd974ff1096c42ab
Maurice Heinze <mheinze@2-I-5.42heilbronn.de>
2022-10-17 14:45:25 +0200

    Signalhandlerfully implemented! Ctrl-\ does nothing, Ctrl-C displays new line, Ctrl-D ends minishell

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include "../inc/minishell.h"
 
-int	g_status;
+// gcc ./src/main.c ./inc/minishell.h ./libft/libft.a ./working_tools/*.c -lreadline -L /goinfre/mheinze/.brew/opt/readline/lib -I /goinfre/mheinze/.brew/opt/readline/include
 
-g_status = 0;
-
+int	g_status = 0;
 
 int	main(int argc, char *argv[])
 {
@@ -14,11 +13,12 @@ int	main(int argc, char *argv[])
 	while (1)
 	{
 		line = readline("\e[1;92mminishell\e[1;94m $\e[0;37m ");
+		if (line == NULL)
+			return (0); // exit shell
 		if (line[0] != '\0')
 			add_history(line);
 		printf("%s\n", line);
 		free(line);
 	}
-	// add_history("test");
 	return (0);
 }
--- working_tools/signal_handler.c
+++ working_tools/signal_handler.c
@@ -6,20 +6,25 @@
 /*   By: mheinze <mheinze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 21:08:49 by mheinze           #+#    #+#             */
-/*   Updated: 2022/10/17 13:45:33 by mheinze          ###   ########.fr       */
+/*   Updated: 2022/10/17 14:43:01 by mheinze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+// should do nothing
 static void	signal_quit(int sig)
 {
-	printf("Quit!");
+	// should do nothing
 }
 
+// should return a new prompt
 static void	signal_interrupt(int sig)
 {
-	printf("Interrupt!");
+	write (1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	setup_signal_handler(void)
@@ -27,3 +32,8 @@ void	setup_signal_handler(void)
 	signal(SIGQUIT, signal_quit); /* Ctrl-\ */
 	signal(SIGINT, signal_interrupt); /* Ctrl-C */
 }
+
+
+//  gcc ./src/main.c ./inc/minishell.h ./libft/libft.a ./working_tools/*.c -lreadline -L /goinfre/mheinze/.brew/opt/readline/lib -I /goinfre/mheinze/.brew/opt/readline/include
+
+// -lreadline -L .brew/opt/readline/lib -I .brew/opt/readline/include
