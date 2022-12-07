563de4b3f94321a59e48effd18b96cf8d573bcaa
Maurice Heinze <mheinze@2-I-2.42heilbronn.de>
2022-12-07 14:51:31 +0100

    Fixed norminette - split_line

--- parser/split_line.c
+++ parser/split_line.c
@@ -6,7 +6,7 @@
 /*   By: mheinze <mheinze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 21:00:11 by mheinze           #+#    #+#             */
-/*   Updated: 2022/12/06 21:02:50 by mheinze          ###   ########.fr       */
+/*   Updated: 2022/12/07 14:50:02 by mheinze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,46 +24,52 @@ static char	*setup_split_line(char *str, int *i, int *start, int *no_word)
 	return (tmp);
 }
 
+static char	*handle_quote(char *str, int *i, int *start, int *no_word)
+{
+	if (str[(*i) + quote_length(str[(*i)], str, (*i)) + 2] == ' ')
+	{
+		(*i) = (*i) + quote_length(str[(*i)], str, (*i)) + 2;
+		return (ft_substr(str, (*start), (*i) - (*start) + 1));
+	}
+	else
+	{
+		(*i) = (*i) + quote_length(str[(*i)], str, (*i)) + 1;
+		return (ft_substr(str, (*start), (*i) - (*start) + 1));
+	}
+}
+
+static char	*add_word(char *str, int *i, int *start, int *no_word)
+{
+	if (ft_strchr(" 	", str[(*i)]))
+		(*i)++;
+	return (ft_substr(str, (*start), (*i) - (*start)));
+}
+
 char	**split_line(char *str)
 {
 	int		i;
 	int		start;
 	int		no_word;
-	int		total_words;
 	char	**words;
 
 	str = setup_split_line(str, &i, &start, &no_word);
-	total_words = count_words(str);
-	words = malloc(sizeof(char *) * (total_words + 1));
+	words = malloc(sizeof(char *) * (count_words(str) + 1));
 	while (str[i] != '\0')
 	{
 		start = i;
-		while (str[i] != '\'' && str[i] != '\"' && str[i] != ' ' && str[i] != '\0')
+		while (str[i] != '\'' && str[i] != '\"'
+			&& str[i] != ' ' && str[i] != '\0')
 			i++;
 		if (i > start)
 		{
-			if (ft_strchr(" 	", str[i]))
-				i++;
-			words[no_word++] = ft_substr(str, start, i - start);
+			words[no_word++] = add_word(str, &i, &start, &no_word);
 			continue ;
 		}
 		if (str[i] == '\'' || str[i] == '\"')
-		{
-			if (str[i + quote_length(str[i], str, i) + 2] == ' ')
-			{
-				i = i + quote_length(str[i], str, i) + 2;
-				words[no_word++] = ft_substr(str, start, i - start + 1);
-			}
-			else
-			{
-				i = i + quote_length(str[i], str, i) + 1;
-				words[no_word++] = ft_substr(str, start, i - start + 1);
-			}
-		}
+			words[no_word++] = handle_quote(str, &i, &start, &no_word);
 		i++;
 	}
 	free(str);
-	str = NULL;
 	words[no_word] = NULL;
 	return (words);
 }
