e6751ed9e401c0c8bcabe61b818b06bd8398ddf0
Maurice Heinze <mheinze@2-I-5.42heilbronn.de>
2022-12-16 16:11:14 +0100

    Fixed double free error for empty quotes

--- utils/check_line.c
+++ utils/check_line.c
@@ -6,7 +6,7 @@
 /*   By: mheinze <mheinze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 17:08:32 by mheinze           #+#    #+#             */
-/*   Updated: 2022/12/15 17:10:18 by mheinze          ###   ########.fr       */
+/*   Updated: 2022/12/16 16:10:19 by mheinze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ int	check_line(char *line)
 
 	i = 0;
 	while (line[i] == ' ' || line[i] == '	'
-		|| line[i] == '.' || line[i] == ';')
+		|| line[i] == '.' || line[i] == ';'
+		|| line[i] == '\"' || line[i] == '\'')
 		i++;
 	if (line[i] == '\0')
 	{
 		cmd = ft_strtrim(line, " 	");
+		track_history(line);
 		printf("minishell: %s: command not found\n", cmd);
 		free(line);
 		line = NULL;
